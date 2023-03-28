e91958f880353037e7efdfd5c8efda7c741a1e2b
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-03-28 21:27:04 +0300

    - Import update queries from data.sql - Use group by to shorten queries

--- queries.sql
+++ queries.sql
@@ -1,12 +1,41 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+BEGIN;
+
+UPDATE animals SET SPECIES='unspecified';
+
+ROLLBACK;
+
+BEGIN;
+
+UPDATE animals SET SPECIES='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET SPECIES='pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+BEGIN;
+
+DELETE FROM animals WHERE dob > '2022-01-01';
+
+SAVEPOINT del_jan_2022;
+
+UPDATE animals SET weight_kg=weight_kg * -1;
+
+ROLLBACK TO del_jan_2022;
+
+UPDATE animals SET weight_kg=weight_kg * -1 WHERE sign(weight_kg) < 1;
+
+COMMIT;
+
 SELECT COUNT(*) as total_number FROM animals;
 SELECT COUNT(*) as less_than_one_escapes FROM animals WHERE escape_attempts < 1;
 SELECT AVG(weight_kg) as avg_weight FROM animals;
 SELECT neutered, avg(escape_attempts) as escapes FROM animals GROUP BY neutered;
-SELECT MAX(weight_kg) as digimon_max_wght FROM animals WHERE SPECIES='digimon';
-SELECT MAX(weight_kg) as digimon_min_wght FROM animals WHERE SPECIES='pokemon';
-SELECT MIN(weight_kg) as pokemon_max_wght FROM animals WHERE SPECIES='digimon';
-SELECT MIN(weight_kg) as pokemon_min_wght FROM animals WHERE SPECIES='pokemon';
-SELECT AVG(escape_attempts) as digimon_avg_escapes FROM animals WHERE SPECIES='digimon' AND DOB BETWEEN '1990-01-01' AND '2000-01-01';
-SELECT AVG(escape_attempts) as pokemon_avg_escapes FROM animals WHERE SPECIES='pokemon' AND DOB BETWEEN '1990-01-01' AND '2000-01-01';
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE dob >= '1990-01-01' AND dob <= '2000-12-31' GROUP BY species;
